f4dddbba070338a1f5d977fff574adc3472fd46a
ahmad-TG <ahmadroyani913@gmail.com>
2019-10-26 10:10:07 +0700

    upadete 1

--- common/models/admin.js
+++ common/models/admin.js
@@ -1,32 +1,32 @@
 'use strict';
 
 module.exports = function(Admin) {
-    Admin.getAdminByName = function(name, callback){
-        new Promise(function(resolve, reject) {
-            //find name
-            Admin.find({where :{nama : {like : name}}}, function(err, result){
-                if (err) reject (err);
-                if (result === null){
-                    err = new Error("User Not Found");
-                    err.statusCode =  404;
-                    reject (err)
-                }
-                resolve(result);
+    Admin.getAdminByName = function(name,  callback) {
+            new Promise(function(resolve, reject) {
+                //find name
+                Admin.find( {where :{username : {like : name}}}, function(err, result){
+                    if (err) reject (err);
+                    if (result === null){
+                        err = new Error("User Not Found");
+                        err.statusCode =  404;
+                        reject (err)
+                    }
+                    resolve(result);
+                });
+            }).then(function(res) {
+                if (!res) callback (err);
+                return callback(null, res);
+            }).catch(function (err) {
+                callback (err);
             });
-        }).then(function(res) {
-            if (!res) callback (err);
-            return callback(null, res[0]);
-        }).catch(function (err) {
-            callback (err);
-        });
-    };
+        };
 
     Admin.remoteMethod(
         'getAdminByName',
         {
             description : 'get user by name',
             accepts: [
-                {arg : 'name', type : 'string'}
+                {arg : 'password', type : 'string'}
             ],
             returns : {
                 arg : 'res', type: 'object', root: true
@@ -36,3 +36,24 @@ module.exports = function(Admin) {
     );
 
 };
+
+
+// Admin.getAdminByName = function(name, callback) {
+//     new Promise(function(resolve, reject) {
+//         //find name
+//         Admin.find( {where :{username : {like : name}}}, function(err, result){
+//             if (err) reject (err);
+//             if (result === null){
+//                 err = new Error("User Not Found");
+//                 err.statusCode =  404;
+//                 reject (err)
+//             }
+//             resolve(result);
+//         });
+//     }).then(function(res) {
+//         if (!res) callback (err);
+//         return callback(null, res);
+//     }).catch(function (err) {
+//         callback (err);
+//     });
+// };
